dd767abfe019a135b035f36abdc713a073a16046
Seungwoo Jung <seungwoojung@Seungwooui-Macmini.local>
2021-01-20 05:03:58 +0900

    이메일 중복체크 로직 오류 수정

--- services/auth/handler/signUp/emailDuplicate.ts
+++ services/auth/handler/signUp/emailDuplicate.ts
@@ -1,23 +1,56 @@
 'use strict';
 
+import * as AWS from "aws-sdk";
 import { APIGatewayProxyHandler } from "aws-lambda";
 import SubpingDDB from "subpingddb";
 
 import { success, failure } from "../../libs/response-lib";
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
+    const subpingDDBCore = new SubpingDDB(process.env.subpingTable);
+    const coreController = subpingDDBCore.getController();
+
+    const _getUser = async (email) => {
+        const cognitoProvider = new AWS.CognitoIdentityServiceProvider({
+            region: "ap-northeast-2",
+        });
+
+        const userCognito = await new Promise((resolve, reject) => cognitoProvider.adminGetUser({
+            UserPoolId: "ap-northeast-2_VzCrSsELb",
+            Username: email
+        }, (err, data) => {
+            if (err) {
+                switch (err.code) {
+                    case "UserNotFoundException":
+                        resolve(null)
+                        break
+                    default:
+                        console.log(err)
+                        throw new Error("cognitoException");
+                }
+            }
+
+            resolve(data)
+        }))
+
+        const userDB = (await coreController.read("model-PK-Index", email, "user")).Items[0]
+
+        return {
+            cognito: userCognito,
+            db: userDB
+        };
+    }
+
     try {
         const body = JSON.parse(event.body || "");
         const email = body.email;
+        
+        const user = await _getUser(email);
 
-        const subpingDDB = new SubpingDDB(process.env.subpingTable);
-        const controller = subpingDDB.getController();
-        const userDB = (await controller.read("model-PK-Index", "user", email)).Items[0];
-
-        if (userDB) {
+        if (!(user.db && user.cognito)) {
             return success({
                 success: false,
-                message: "emailExistException"
+                message: "EmailExistException"
             })
         }
 
@@ -30,7 +63,7 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
     catch (e) {
         return failure({
             success: false,
-            message: "emailDuplicateException"
+            message: "EmailDuplicateException"
         })
     }
 };
