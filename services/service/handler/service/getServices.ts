f7e09b22a19f91d244b9c320830a2e3d7e1fe907
W1nU <jsw9808@gmail.com>
2021-07-26 17:15:32 +0900

    기능 : getServices RDB 수정

--- services/service/handler/service/getServices.ts
+++ services/service/handler/service/getServices.ts
@@ -1,50 +1,36 @@
-import SubpingDDB from "subpingddb";
+import SubpingRDB, { Repository, Entity } from "subpingrdb";
 import ServiceModel from "subpingddb/model/subpingTable/service"
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 import { success, failure } from "../../libs/response-lib";
 
-interface Category {
-    [key:string]: Array<ServiceModel>
-}
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
     try {
-        let response: Category = {};
+        let response = [];
         const body = JSON.parse(event.body || "");
         
-        // 특정 카테고리에 대한 요청이 있을 경우
         const requestedCategory = body.category || null;
 
-        const subpingDDB = new SubpingDDB(process.env.subpingTable);
-        const controller = subpingDDB.getController();
-        
-        if(!requestedCategory) {
-            const services = (await controller.read("model-PK-Index", "service")).Items;
-        
-            for(const service of services){
-                if (!Object.keys(response).includes(service.serviceCategory)){
-                    response[service.serviceCategory] = [service];
-                }
-    
-                else {
-                    response[service.serviceCategory].push(service);
-                }
-            }
-        } 
+        const subpingRDB = new SubpingRDB();
+        const connection = await subpingRDB.getConnection("dev");
+        const categoryRepository = connection.getCustomRepository(Repository.CategoryRepository);
+
+        if(requestedCategory) {
+            response = await categoryRepository.findServiceByCategory(requestedCategory)
+
+            return success({
+                success: true,
+                message: response  
+            })
+        }
 
         else {
-            const services = (await controller.readWithFilter("model-PK-Index", "service", null, {
-                serviceCategory: requestedCategory
-            })).Items;
-            
-            response[requestedCategory] = services;
+            return failure({
+                success: false,
+                message: "NoRequestedCategoryException"
+            })
         }
-        
-        return success({
-            success: true,
-            message: response  
-        })
     }
 
     catch (e) {
