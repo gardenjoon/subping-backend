d5c46a9804998bb87fcf0fa36b9455478ea010bf
W1nU <jsw9808@gmail.com>
2021-08-27 15:32:22 +0900

    수정 : getServices 수정

--- modules/SubpingRDB/src/repository/Service.ts
+++ modules/SubpingRDB/src/repository/Service.ts
@@ -17,7 +17,7 @@ export class ServiceRepository extends Repository<Service> {
     }
 
     async deleteService(name: string): Promise<void> {
-        await this.delete({ name : name });
+        await this.delete({ name: name });
     }
 
     async findByName(name: string) {
@@ -26,143 +26,126 @@ export class ServiceRepository extends Repository<Service> {
             .getMany();
     }
 
-    async getServices(options?:{
-            category?: boolean,
-            tag?: boolean,
-            rank?: boolean,
-            standardDate?: string,
-            standardTime?: string,
-            like?: boolean,
-            userEmail?: string,
-            pagination?: {
-                limit: number,
-                page: number
-            }
-        }) {
-        
-        const { category, tag, rank, standardDate, standardTime, like, userEmail, pagination: { limit, page } } = options;
-        
+    async getServices(options?: {
+        category?: boolean,
+        tag?: boolean,
+        period?: boolean,
+        rank?: {
+            standardDate: string,
+            standardTime: string,
+        },
+        like?: {
+            userEmail: string,
+        },
+        pagination?: {
+            limit: number,
+            page: number,
+            standardTime: string
+        }
+        filter?: {
+            categoryName?: string,
+            serviceId?: string,
+        }
+
+    }) {
+        const { category, tag, period, rank, like, pagination, filter } = options;
+
         let query = this.createQueryBuilder("service")
 
         query = query.select("service.*");
 
-        if(category) {
-            query = query
-                .addSelect("GROUP_CONCAT(DISTINCT serviceCategory.categoryName)", "category")
-                .innerJoin("service.serviceCategories", "serviceCategory");
-        }
-        
-        if(tag) {
-            query = query
-              .addSelect("GROUP_CONCAT(DISTINCT serviceTag.tag)", "tag")
-              .innerJoin("service.serviceTags", "serviceTag")
-        }
-        
-        if(rank) {
-            if(standardTime && standardDate) {
+        if (category) {
+            if (filter && filter.categoryName) {
                 query = query
-                .addSelect("serviceRank.rank", "rank")
-                .innerJoin("service.serviceRank", "serviceRank", 
-                `serviceRank.date = "${standardDate}" 
-                AND serviceRank.time = "${standardTime}"`)
-                .orderBy("serviceRank.rank");
+                    .addSelect("GROUP_CONCAT(DISTINCT serviceCategory.categoryName)", "category")
+                    .innerJoin("service.serviceCategories", "serviceCategory", `serviceCategory.categoryName LIKE "%${filter.categoryName}%"`);
+
+            } else {
+                query = query
+                    .addSelect("GROUP_CONCAT(DISTINCT serviceCategory.categoryName)", "category")
+                    .innerJoin("service.serviceCategories", "serviceCategory");
             }
-            
+        }
+
+        if (tag) {
+            query = query
+                .addSelect("GROUP_CONCAT(DISTINCT serviceTag.tag)", "tag")
+                .innerJoin("service.serviceTags", "serviceTag")
+        }
+
+        if (period) {
+            query = query
+                .addSelect("GROUP_CONCAT(DISTINCT periods.period)", "period")
+                .innerJoin("service.periods", "periods")
+        }
+
+        if (rank) {
+            if (rank.standardTime && rank.standardDate) {
+                query = query
+                    .addSelect("serviceRank.rank", "rank")
+                    .innerJoin("service.serviceRank", "serviceRank",
+                        `serviceRank.date = "${rank.standardDate}" 
+                AND serviceRank.time = "${rank.standardTime}"`)
+                    .orderBy("serviceRank.rank");
+            }
+
             else {
                 throw new Error("[SubpingRDB] getServices Rank가 정의되었지만 기준시간이 없습니다.");
             }
         }
 
-        if(like) {
-            if(userEmail) {
+        if (like) {
+            if (like.userEmail) {
                 query = query
                     .addSelect("IF(userLike.createdAt IS NULL, False, True)", "like")
-                    .leftJoin("service.userLikes", "userLike", `userLike.user = "${userEmail}"`)
+                    .leftJoin("service.userLikes", "userLike", `userLike.user = "${like.userEmail}"`)
             }
 
             else {
                 throw new Error("[SubpingRDB] getServices Like가 정의되었지만 userEmail이 없습니다.")
             }
         }
-        
-        if(limit && page && standardTime) {
-            query = query
-                .andWhere(`service.createdAt < "${standardTime}"`)
-                .orderBy("service.createdAt", "ASC")
-                .offset(limit*(page-1))
-                .limit(limit)
+
+        if (pagination) {
+            if (pagination.limit && pagination.page && pagination.standardTime) {
+                query = query
+                    .andWhere(`service.createdAt < "${pagination.standardTime}"`)
+                    .orderBy("service.createdAt", "ASC")
+                    .offset(pagination.limit * (pagination.page - 1))
+                    .limit(pagination.limit)
+            }
         }
 
-        if(category || tag || rank || like) {
+        if (filter) {
+            if (filter.serviceId) {
+                query = query.where(`service.id = "${filter.serviceId}"`)
+            }
+        }
+
+        if (category || tag || rank || like || period) {
             query = query.groupBy("service.id")
         }
 
         const result = await query
             .getRawMany();
 
-        if(category || tag) {
+        if (category || tag || period) {
             result.map(service => {
-                if(category) {
+                if (category) {
                     service.category = service.category.split(",");
                 }
 
-                if(tag) {
+                if (tag) {
                     service.tag = service.tag.split(",");
+                }
+
+                if (period) {
+                    service.period = service.period.split(",");
                 }
             })
         }
-    
+
         return result;
-        
-    }
 
-    async getServicesWithCategory(category: string, userEmail: string) {
-        const serviceCategoryRepository = this.manager.getCustomRepository(ServiceCategoryRepository);
-
-        const getAllServiceSQL = serviceCategoryRepository.createQueryBuilder("serviceCategory")
-            .select("service.*")
-            .addSelect("GROUP_CONCAT(DISTINCT serviceCategory.categoryName)", "category")
-            .addSelect("GROUP_CONCAT(DISTINCT serviceTag.tag)", "tag")
-            .addSelect("IF(userLike.createdAt IS NULL, False, True)", "like")
-            .innerJoin("serviceCategory.service", "service")
-            .innerJoin("service.serviceTags", "serviceTag", "service.id = serviceTag.serviceId")
-            .leftJoin("service.userLikes", "userLike", `userLike.user = "${userEmail}"`)
-            .groupBy("service.id")
-            .getSql();
-
-        const servicesOfCategory = await this.manager.createQueryBuilder()
-            .select("s.*")
-            .from("(" + getAllServiceSQL + ")", "s")
-            .where(`s.category LIKE "%${category}%"`)
-            .getRawMany();
-
-        servicesOfCategory.map(service => {
-            service.tag = service.tag.split(",");
-            service.category = service.category.split(",");
-        });
-
-        return servicesOfCategory;
-    }
-
-    async getServiceWithId(id: string, userEmail: string) {
-        const service = await this.createQueryBuilder("service")
-            .select("service.*")
-            .addSelect("GROUP_CONCAT(DISTINCT serviceCategory.categoryName)", "category")
-            .where(`service.id = "${id}"`)
-            .innerJoin("service.serviceCategories", "serviceCategory")
-            .addSelect("GROUP_CONCAT(DISTINCT serviceTag.tag)", "tag")
-            .innerJoin("service.serviceTags", "serviceTag")
-            .addSelect("IF(userLike.createdAt IS NULL, False, True)", "like")
-            .leftJoin("service.userLikes", "userLike", `userLike.user = "${userEmail}"`)
-            .addSelect("GROUP_CONCAT(DISTINCT periods.period)", "period")
-            .innerJoin("service.periods", "periods")
-            .groupBy("service.id")
-            .getRawOne();
-
-        service.tag = service.tag.split(",");
-        service.category = service.category.split(",");
-        service.period = service.period.split(",");
-
-        return service;
     }
 }
