2ac2f5cc1689d236601080d43468948f660bf2ad
gardenjoon <wees114@gmail.com>
2021-11-12 12:50:45 +0900

    수정 : hotchart, service 버그

--- services/service/handler/chart/cronMakeRank.ts
+++ services/service/handler/chart/cronMakeRank.ts
@@ -2,39 +2,41 @@ import SubpingRDB, { Entity, Repository } from "subpingrdb";
 import SubpingDDB from "../../libs/subpingddb";
 import HotChartTimeModel from "../../libs/subpingddb/model/subpingTable/hotChartTime";
 
-import * as moment from "moment-timezone";
+import moment from "moment-timezone";
 
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler } from "aws-lambda";
 import { success, failure } from "../../libs/response-lib";
 
-export const handler:APIGatewayProxyHandler  = async (_event, _context) => {
+export const handler: APIGatewayProxyHandler = async (_event, _context) => {
     const makeHour = (hour: number) => {
-        let standardHour = null;
-        return standardHour = 
-                (3 <= hour && hour < 9) ? "03:00"
-            :   (9 <= hour && hour < 15) ? "09:00"
-            :   (15 <= hour && hour < 21) ? "15:00"
-            :   "21:00"
-    }
+        let standardHour = 
+            3 <= hour && hour < 9 ? "03:00"
+        :   9 <= hour && hour < 15 ? "09:00"
+        :   15 <= hour && hour < 21 ? "15:00"
+        : "21:00";
+
+        return standardHour
+    };
 
     const setTime = () => {
         const time = {
-            currentHour : null,
-            currentDate : null,
-            standardHour : null,
-            standardDate : null
-        }
+            currentHour: null,
+            currentDate: null,
+            standardHour: null,
+            standardDate: null,
+        };
 
         const currentTime = moment();
+
         time.currentHour = makeHour(currentTime.hours());
         time.currentDate = currentTime.format("YYYY-MM-DD");
 
         let standardTime = currentTime.subtract(6, "hours");
         time.standardHour = makeHour(standardTime.hours());
-        time.standardDate = standardTime.format("YYYY-MM-DD") ;
+        time.standardDate = standardTime.format("YYYY-MM-DD");
 
         return time;
-    }
+    };
 
     try {
         const subpingDDB = new SubpingDDB(process.env.subpingTable);
@@ -43,9 +45,13 @@ export const handler:APIGatewayProxyHandler  = async (_event, _context) => {
         const connection = await subpingRDB.getConnection("dev");
 
         const time = setTime();
-        
+
+        const serviceRepository = connection.getCustomRepository(Repository.Service);
+        const allServices = await serviceRepository.queryAllServices();
+
         const serviceEventRepository = connection.getCustomRepository(Repository.ServiceEvent);
         const servicesWithEvent = await serviceEventRepository.queryServiceEvents(time.standardDate, time.standardHour);
+
         const serviceEventModel = new Entity.ServiceEvent();
         const serviceRankModel = new Entity.ServiceRank();
 
@@ -66,51 +72,47 @@ export const handler:APIGatewayProxyHandler  = async (_event, _context) => {
         };
 
         const queryRunner = connection.createQueryRunner();
-
         try {
             await queryRunner.startTransaction();
 
             for (const [index, service] of servicesWithEvent.entries()) {
                 const serviceModel = new Entity.Service();
-                serviceModel.id = service.serviceId
+                serviceModel.id = service.serviceId;
 
                 serviceRankModel.service = serviceModel;
-                serviceRankModel.rank = index+1;
+                serviceRankModel.rank = index + 1;
 
                 await queryRunner.manager.save(serviceRankModel);
             }
 
-            for (const service of servicesWithEvent){
+            for (const service of allServices) {
                 const serviceModel = new Entity.Service();
-                serviceModel.id = service.serviceId
+                serviceModel.id = service.id;
 
                 serviceEventModel.service = serviceModel;
                 await queryRunner.manager.save(serviceEventModel);
-            };
+            }
 
             await controller.create<HotChartTimeModel>(HotChartTimeModel);
 
             await queryRunner.commitTransaction();
-        }
-        catch(e) {
+        } catch (e) {
             console.log(e);
             await queryRunner.rollbackTransaction();
-        }
-        finally {
+        } finally {
             await queryRunner.release();
         }
 
         return success({
             success: true,
-            message: "cronMakeRankSuccess"
+            message: "cronMakeRankSuccess",
+        });
+    } 
+    catch (e) {
+        console.log(e);
+        return failure({
+            success: false,
+            message: "cronMakeRankException",
         });
     }
-
-    catch (e) {
-      console.log(e);
-      return failure({
-          success: false,
-          message: "cronMakeRankException"
-      });
-    }
-}
+};
